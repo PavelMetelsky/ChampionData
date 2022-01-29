8ec0d3180425eeead8b25a5fba741785511cac7b
Pavel Metelsky <p.metelsky@itransition.com>
2022-01-29 17:24:15 +0300

    fix the program

--- DebugTest/Program.cs
+++ DebugTest/Program.cs
@@ -7,15 +7,21 @@ namespace DebugTest
 {
     class Program
     {
-        private const string LocationUri = "https://www.metaweather.com/api/location/search/?query=";
+        private const string LocationUriByLocationName = "https://www.metaweather.com/api/location/search/?query=";
+        private const string LocationUriByCoOrdinates = "https://www.metaweather.com/api/location/search/?lattlong=";
         private const string LocationUri2 = "https://www.metaweather.com/api/location/";
         
         static void Main(string[] args)
         {
+            var location = "minsk"; // GetLocation()
+            
             Print("Enter location:");
-            var location = Console.ReadLine();
+            location = Console.ReadLine();
 
-            var content = GetDataFromWeatherByUri($"{LocationUri}{location}");
+            var content = location.Contains(".")
+                ? GetDataFromWeatherByUri($"{LocationUriByCoOrdinates}{location}")
+                : GetDataFromWeatherByUri($"{LocationUriByLocationName}{location}");
+            
             var id = content.Split("woeid\":")[1].Split(",")[0];
 
             while (true)
@@ -24,10 +30,24 @@ namespace DebugTest
 
                 Print($"{location} max temp: {weatherInfo.consolidated_weather[0].max_temp} min temp: {weatherInfo.consolidated_weather[0].min_temp}");
 
-                var streamWriter = new StreamWriter("weather" + location + DateTimeOffset.Now.ToUnixTimeSeconds() + ".json");
-                streamWriter.Write(JsonSerializer.Serialize(weatherInfo));
+                //{DateTimeOffset.Now.ToUnixTimeSeconds()}
+                var path = $"weather-{location}.json";
+                string docPath = "C:\\Users\\Admah\\Documents\\TestProject\\ChampionData\\DebugTest\\Results";
+                
+                using (StreamWriter streamWriter =  new StreamWriter(Path.Combine(docPath, path), true))
+                {
+                    streamWriter.WriteLine(JsonSerializer.Serialize(weatherInfo));
+                }
+                
                 System.Threading.Thread.Sleep(2000);
             }
+        }
+
+        private static string GetLocation()
+        {
+            Print("Enter location:");
+            
+            return Console.ReadLine();
         }
 
         private static string GetDataFromWeatherByUri(string uri)
